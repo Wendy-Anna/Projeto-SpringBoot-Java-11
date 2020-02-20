24307f13ec61cc06c2c752d412a3b60f83a803a8
Wendy-Anna <wendyufpb@gmail.com>
2020-02-20 14:45:18 -0300

    Revert "Order, Instant, ISO 8601 - PARTE"
    This reverts commit 5ff070f3258a67408ea4cfdd4f493dd2ac988ad4.

--- src/main/java/projetowendy/curso/entities/User.java
+++ src/main/java/projetowendy/curso/entities/User.java
@@ -1,18 +1,13 @@
 package projetowendy.curso.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -24,9 +19,6 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-	
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -78,10 +70,6 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
-	}
-	
-	public List<Order> getOrders() {
-		return orders;
 	}
 
 	@Override
