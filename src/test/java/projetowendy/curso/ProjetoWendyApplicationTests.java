d9cb1de5390c777da4cf50b9c8604009576fe8f0
Wendy-Anna <wendyufpb@gmail.com>
2020-02-20 09:05:48 -0300

    Project created

---
+++ src/test/java/projetowendy/curso/ProjetoWendyApplicationTests.java
@@ -0,0 +1,13 @@
+package projetowendy.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWendyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
