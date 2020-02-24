8216d589dcbfbd25fbc5ea09987a536e3674178d
Wendy-Anna <wendyufpb@gmail.com>
2020-02-24 17:00:27 -0300

    ajustes

--- src/main/java/projetowendy/curso/entities/Category.java
+++ src/main/java/projetowendy/curso/entities/Category.java
@@ -10,9 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
